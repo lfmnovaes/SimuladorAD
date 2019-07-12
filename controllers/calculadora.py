0037a4909e2f3f4378b95521b746658b43b00759
gustavo <andregus@gmail.com>
2019-07-12 01:48:25 -0300

    Ajustes IC Variancia

--- controllers/calculadora.py
+++ controllers/calculadora.py
@@ -39,44 +39,36 @@ class Calculadora(object):
         return (inf, sup, centro, self.ok)
 
 
-    def ICDaVariacia(self, mean_list):
-        #esse método utilizará a formula do qui-quadrado para medir a variancia
+    def ICVariancia(self, lista_de_medias):
+        # Qui-quadrado para medir a variancia
+        # Quantidade de amostras
+        n = len(lista_de_medias)
 
-        #qtd de amostras
-        n = len(mean_list)
+        # Média das amostras
+        media = np.sum(lista_de_medias)/n
 
-        #média amostral
-        mean = np.sum(mean_list)/n
+        # Usando função auxiliar (chi2.isf) para calcular o valor para n = 3200
+        Qui_alpha2 = chi2.isf(q=0.025, df=n-1)
+        Qui1_menosalpha2 = chi2.isf(q=0.975, df=n-1)
 
-        #dados obtidos na tabela da qui-quadrada para alpha=0.5, alpha/2 = 0.25
-        #Qalpha2 = 74.222
-        #Q1menosalpha2 = 129.561
+        # Variância das amostras:
+        # SOMA((Media - Media das Amostras)^2) = S^2
+        s_quadrado = np.sum([(float(element) - float(media))**2 for element in lista_de_medias])/(n-1.0)
 
-        #como na tabela de qui-quadrado só temos ate 100 graus de liberdade, tivemos que usar uma funcao
-        #auxiliar para calcular o valor dela para n = 3200
+        # Calculo do Intervalo de Confiança para qui-quadrado
+        inf = (n-1)*s_quadrado/Qui1_menosalpha2
+        sup = (n-1)*s_quadrado/Qui_alpha2
+        centro = inf + (sup - inf)/2.0
 
-        Qalpha2 = chi2.isf(q=0.025, df=n-1)
-        Q1menosalpha2 = chi2.isf(q=0.975, df=n-1)
-
-        #variancia amostral = SUM((Media - Media Amostral)^2) = S^2
-        s_quadrado = np.sum([(float(element) - float(mean))**2 for element in mean_list])/(n-1.0)
-
-        #calculo do Intervalo de Confiança pela qui-quadrado
-        lower = (n-1)*s_quadrado/Q1menosalpha2
-        upper = (n-1)*s_quadrado/Qalpha2
-
-        center = lower + (upper - lower)/2.0
-
-        if center/10.0 < (upper - lower):
-            #print center/10.0
-            #print upper - lower
-            self.aprovado = False
-            #print "teste IC da variancia não obteve precisao de 5%, intervalo maior do que 10% do valor central"
+        # Aqui nós verificamos se obtivemos uma precisão de 5%
+        # Se intervalo for maior do que 10% do valor central, não atingiu precisão adequada
+        if centro/10.0 < (sup - inf):
+            self.ok = False
         else:
-            self.aprovado = True
+            self.ok = True
 
-        #retorna o limite inferior, limite superior, o valor central e a precisão, nessa ordem.
-        return (lower, upper, center, self.aprovado)
+        #retorna o limite inferior, limite superior, o valor central e se está dentro do intervalo
+        return (inf, sup, centro, self.ok)
 
     #c.plotGrafico(x, y, x_legenda, y_legenda)
     def plotGrafico(self, x, y, x_legenda='x', y_legenda='y', saida='plot'):
